18fcadc7faf9c63787288e708e278eda6a4154bc
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-02-19 08:17:27 +0900

    Improve: レスポンシブ対応
    ・共通レイアウトを修正
    ・scrolltrigerの発火をPCサイズのみに変更

--- _static/src/assets/js/modules/horizontalScroll.js
+++ _static/src/assets/js/modules/horizontalScroll.js
@@ -3,27 +3,33 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-window.addEventListener("load", function () {
-  const area = document.querySelector(".js-area");
-  const wrap = document.querySelector(".js-wrap");
-  const items = document.querySelectorAll(".js-item");
-  const num = items.length;
+const windowWidth = window.innerWidth;
 
-  gsap.set(wrap, { width: num * 100 + "%" });
-  gsap.set(items, { width: 100 / num + "%" });
+const breakPoint = 1024;
 
-  //ここから追加
-  gsap.to(items, {
-    xPercent: -100 * (num - 1),
-    ease: "none",
-    scrollTrigger: {
-      trigger: area,
-      start: "top top",
-      end: "bottom top",
-      pin: true,
-      scrub: true,
-      markers: true,
-    },
+const isPcSize = windowWidth > breakPoint;
+
+if (isPcSize) {
+  window.addEventListener("load", function () {
+    const area = document.querySelector(".js-area");
+    const wrap = document.querySelector(".js-wrap");
+    const items = document.querySelectorAll(".js-item");
+    const num = items.length;
+
+    gsap.set(wrap, { width: num * 100 + "%" });
+    gsap.set(items, { width: 100 / num + "%" });
+
+    gsap.to(items, {
+      xPercent: -100 * (num - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: area,
+        start: "top top",
+        end: "bottom top",
+        pin: true,
+        scrub: true,
+        markers: true,
+      },
+    });
   });
-  //ここまで追加
-});
+}
